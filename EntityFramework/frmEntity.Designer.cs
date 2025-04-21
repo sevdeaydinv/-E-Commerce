9dbe28894eb7affbabaa1eeef8d29b3f09cc8512
sevdeaydinv <sevdesila05@icloud.com>
2025-04-22 02:29:44 +0300

    EntityFramewoek eklendi

---
+++ EntityFramework/frmEntity.Designer.cs
@@ -0,0 +1,321 @@
+﻿namespace EntityFramework
+{
+    partial class frmEntity
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtAdiSoyadi = new System.Windows.Forms.TextBox();
+            this.txtEposta = new System.Windows.Forms.TextBox();
+            this.txtSifre = new System.Windows.Forms.TextBox();
+            this.txtRol = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.btnDuzenle = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.btnTemizle = new System.Windows.Forms.Button();
+            this.btnCikis = new System.Windows.Forms.Button();
+            this.txtEpostaAra = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(800, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Entity Framework ile Veritabnı İşlemleri";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 25);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(800, 425);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridView1.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellDoubleClick);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.txtEpostaAra);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.btnCikis);
+            this.panel1.Controls.Add(this.btnTemizle);
+            this.panel1.Controls.Add(this.btnSil);
+            this.panel1.Controls.Add(this.btnDuzenle);
+            this.panel1.Controls.Add(this.btnEkle);
+            this.panel1.Controls.Add(this.dateTimePicker1);
+            this.panel1.Controls.Add(this.txtSifre);
+            this.panel1.Controls.Add(this.txtEposta);
+            this.panel1.Controls.Add(this.txtAdiSoyadi);
+            this.panel1.Controls.Add(this.txtRol);
+            this.panel1.Controls.Add(this.txtId);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 250);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 200);
+            this.panel1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "KullanıcıID:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(154, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "AdSoyad:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(397, 67);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Eposta:";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(655, 67);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(31, 13);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Şifre:";
+            this.label5.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 105);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(26, 13);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Rol:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(491, 101);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(91, 13);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "OluşturulmaTarihi:";
+            this.label7.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(75, 64);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(58, 20);
+            this.txtId.TabIndex = 1;
+            // 
+            // txtAdiSoyadi
+            // 
+            this.txtAdiSoyadi.Location = new System.Drawing.Point(213, 64);
+            this.txtAdiSoyadi.Name = "txtAdiSoyadi";
+            this.txtAdiSoyadi.Size = new System.Drawing.Size(178, 20);
+            this.txtAdiSoyadi.TabIndex = 1;
+            // 
+            // txtEposta
+            // 
+            this.txtEposta.Location = new System.Drawing.Point(446, 64);
+            this.txtEposta.Name = "txtEposta";
+            this.txtEposta.Size = new System.Drawing.Size(203, 20);
+            this.txtEposta.TabIndex = 1;
+            // 
+            // txtSifre
+            // 
+            this.txtSifre.Location = new System.Drawing.Point(692, 64);
+            this.txtSifre.Name = "txtSifre";
+            this.txtSifre.Size = new System.Drawing.Size(74, 20);
+            this.txtSifre.TabIndex = 1;
+            // 
+            // txtRol
+            // 
+            this.txtRol.Location = new System.Drawing.Point(41, 101);
+            this.txtRol.Multiline = true;
+            this.txtRol.Name = "txtRol";
+            this.txtRol.Size = new System.Drawing.Size(213, 25);
+            this.txtRol.TabIndex = 1;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.Location = new System.Drawing.Point(588, 98);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(178, 20);
+            this.dateTimePicker1.TabIndex = 2;
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(15, 135);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(95, 39);
+            this.btnEkle.TabIndex = 3;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // btnDuzenle
+            // 
+            this.btnDuzenle.Location = new System.Drawing.Point(116, 135);
+            this.btnDuzenle.Name = "btnDuzenle";
+            this.btnDuzenle.Size = new System.Drawing.Size(95, 39);
+            this.btnDuzenle.TabIndex = 3;
+            this.btnDuzenle.Text = "Düzenle";
+            this.btnDuzenle.UseVisualStyleBackColor = true;
+            this.btnDuzenle.Click += new System.EventHandler(this.btnDuzenle_Click);
+            // 
+            // btnSil
+            // 
+            this.btnSil.Location = new System.Drawing.Point(217, 135);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(95, 39);
+            this.btnSil.TabIndex = 3;
+            this.btnSil.Text = "Sil";
+            this.btnSil.UseVisualStyleBackColor = true;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // btnTemizle
+            // 
+            this.btnTemizle.Location = new System.Drawing.Point(570, 135);
+            this.btnTemizle.Name = "btnTemizle";
+            this.btnTemizle.Size = new System.Drawing.Size(95, 39);
+            this.btnTemizle.TabIndex = 3;
+            this.btnTemizle.Text = "Temizle";
+            this.btnTemizle.UseVisualStyleBackColor = true;
+            this.btnTemizle.Click += new System.EventHandler(this.btnTemizle_Click);
+            // 
+            // btnCikis
+            // 
+            this.btnCikis.Location = new System.Drawing.Point(671, 135);
+            this.btnCikis.Name = "btnCikis";
+            this.btnCikis.Size = new System.Drawing.Size(95, 39);
+            this.btnCikis.TabIndex = 3;
+            this.btnCikis.Text = "Çıkış";
+            this.btnCikis.UseVisualStyleBackColor = true;
+            // 
+            // txtEpostaAra
+            // 
+            this.txtEpostaAra.Location = new System.Drawing.Point(588, 21);
+            this.txtEpostaAra.Name = "txtEpostaAra";
+            this.txtEpostaAra.Size = new System.Drawing.Size(178, 20);
+            this.txtEpostaAra.TabIndex = 5;
+            this.txtEpostaAra.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(477, 24);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(99, 13);
+            this.label8.TabIndex = 4;
+            this.label8.Text = "Epostaya Göre Ara:";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // frmEntity
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Name = "frmEntity";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Entity Framework ile Veritabanı İşlemleri";
+            this.Load += new System.EventHandler(this.frmEntity_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.TextBox txtSifre;
+        private System.Windows.Forms.TextBox txtEposta;
+        private System.Windows.Forms.TextBox txtAdiSoyadi;
+        private System.Windows.Forms.TextBox txtRol;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Button btnDuzenle;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Button btnCikis;
+        private System.Windows.Forms.Button btnTemizle;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.TextBox txtEpostaAra;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ EntityFramework/frmEntity.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EntityFramework
+{
+    public partial class frmEntity: Form
+    {
+        KodlamaDünyasıDBEntities db = new KodlamaDünyasıDBEntities();
+        public frmEntity()
+        {
+            InitializeComponent();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void frmEntity_Load(object sender, EventArgs e)
+        {
+            Listele();
+
+        }
+
+        private void Listele()
+        {
+            dataGridView1.DataSource = db.Kullanıcılar.ToList();
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            var entity = new Kullanıcılar
+            {
+                AdSoyad = txtAdiSoyadi.Text,
+                Eposta = txtEposta.Text,
+                Şifre = txtSifre.Text,
+                Rol = txtRol.Text,
+                OluşturulmaTarihi = dateTimePicker1.Value,
+                 
+            };
+            db.Kullanıcılar.Add(entity);
+            db.SaveChanges();
+            Temizle();
+            Listele();
+        }
+        void Temizle()
+        {
+            txtId.Text = string.Empty;
+            txtAdiSoyadi.Text = string.Empty;
+            txtEposta.Text = string.Empty;
+            txtRol.Text = string.Empty;
+            txtSifre.Text = string.Empty;
+            dateTimePicker1.Value= DateTime.Now;
+        }
+        private void btnTemizle_Click(object sender, EventArgs e)
+        {
+            Temizle();
+        }
+
+        private void btnDuzenle_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(txtId.Text);
+            var model = db.Kullanıcılar.FirstOrDefault(x=>x.KullanıcıID==id);
+            model.AdSoyad = txtAdiSoyadi.Text;
+            model.Eposta = txtEposta.Text;
+            model.Şifre = txtEposta.Text;
+            model.Rol = txtRol.Text;
+            model.OluşturulmaTarihi = dateTimePicker1.Value;
+            db.SaveChanges();
+            Temizle();
+            Listele();
+        }
+
+        private void dataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            txtId.Text = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+            txtAdiSoyadi.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+            txtEposta.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+            txtRol.Text = dataGridView1.CurrentRow.Cells[3].Value.ToString();
+            txtSifre.Text = dataGridView1.CurrentRow.Cells[4].Value.ToString();
+            dateTimePicker1.Text = dataGridView1.CurrentRow.Cells[5].Value.ToString();
+        }
+
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(dataGridView1.CurrentRow.Cells[(0)].Value.ToString());
+            var model = db.Kullanıcılar.FirstOrDefault(x => x.KullanıcıID == id);
+            db.Kullanıcılar.Remove(model);
+            db.SaveChanges();
+            Temizle();
+            Listele();
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = db.Kullanıcılar.Where(x => x.Eposta.Contains(txtEpostaAra.Text)).ToList();
+        }
+    }
+}
